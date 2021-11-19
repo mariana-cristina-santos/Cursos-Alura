e4a0ab45af1bf4e00db1f1ac88d1c06170880e67
mmcrisx <marianacristinasantos05@gmail.com>
2021-11-18 23:58:24 -0300

    criei a rota para criar atendimentos

--- projeto/controllers/atendimentos.js
+++ projeto/controllers/atendimentos.js
@@ -1,4 +1,5 @@
 module.exports = app => { 
-
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
-}
+
+    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um POST'))
+    
